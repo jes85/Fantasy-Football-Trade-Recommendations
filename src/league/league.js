db5aee22a22a79861b0e5a9f28744da10c868338
Jeremy Schreck <schreck.jeremy@gmail.com>
2019-07-30 21:06:49 -0700

    Convert tabs to spaces

--- src/league/league.js
+++ src/league/league.js
@@ -3,107 +3,107 @@ import Trade from '../trade/trade.js';
 
 class League {
     constructor(numWeeksInSeason, startingLineupSlots) {
-	    this.numWeeksInSeason = numWeeksInSeason;
-	    this.startingLineupSlots = startingLineupSlots;
-	}
+      this.numWeeksInSeason = numWeeksInSeason;
+      this.startingLineupSlots = startingLineupSlots;
+  }
 
-	// TODO these trade methods probably don't belong in "League"
-	getAllTrades(currentWeek, teams) {
-		var trades = [];
-		_.each(teams, (team, i) => {
-			// Don't need to calculate trades twice.
-			var otherTeams = teams.slice(i + 1, teams.length);
-			trades = trades.concat(this.getAllTradesForTeam(team, otherTeams, currentWeek));
-		});
-		return trades;
-	}
+  // TODO these trade methods probably don't belong in "League"
+  getAllTrades(currentWeek, teams) {
+    var trades = [];
+    _.each(teams, (team, i) => {
+      // Don't need to calculate trades twice.
+      var otherTeams = teams.slice(i + 1, teams.length);
+      trades = trades.concat(this.getAllTradesForTeam(team, otherTeams, currentWeek));
+    });
+    return trades;
+  }
 
-	getAllTradesForTeam(team, otherTeams, currentWeek) {
-		var trades = [];
-		_.each(otherTeams, (otherTeam) => {
-			var all1v1Trades = this.getAll1v1Trades(team, otherTeam, currentWeek); // getAllnvmTrades(Team1, Team2, 1, 1)
+  getAllTradesForTeam(team, otherTeams, currentWeek) {
+    var trades = [];
+    _.each(otherTeams, (otherTeam) => {
+      var all1v1Trades = this.getAll1v1Trades(team, otherTeam, currentWeek); // getAllnvmTrades(Team1, Team2, 1, 1)
 
-			var all2v2Trades = []; // this.getAll2v2Trades(team, otherTeam, currentWeek); // getAllnvmTrades(Team1, Team2, 2, 2)
+      var all2v2Trades = this.getAll2v2Trades(team, otherTeam, currentWeek); // getAllnvmTrades(Team1, Team2, 2, 2)
 
-			var all2v1Trades = [];//this.getAll2v1Trades(team, otherTeam, currentWeek);
+      var all2v1Trades = this.getAll2v1Trades(team, otherTeam, currentWeek);
 
-			var all1v2Trades = [];//this.getAll2v1Trades(otherTeam, team, currentWeek);
+      var all1v2Trades = this.getAll2v1Trades(otherTeam, team, currentWeek);
 
-			// # 3v3 trades
-			// var all3v3Trades = getAllnvmTrades(Team1, Team2, 3, 3)
-		    trades = trades.concat(all1v1Trades).concat(all2v2Trades).concat(all2v1Trades).concat(all1v2Trades);
-		});
-		return trades;
-	}
+      // # 3v3 trades
+      // var all3v3Trades = getAllnvmTrades(Team1, Team2, 3, 3)
+        trades = trades.concat(all1v1Trades).concat(all2v2Trades).concat(all2v1Trades).concat(all1v2Trades);
+    });
+    return trades;
+  }
 
-	getAll1v1Trades(team1, team2, currentWeek) {
-		var trades = [];
-		_.each(team1.players, (player1) => {
-			_.each(team2.players, (player2) => {
-				var team1AfterTrade = team1.afterTrade([player1], [player2]);
-				var team2AfterTrade = team2.afterTrade([player2], [player1]);
+  getAll1v1Trades(team1, team2, currentWeek) {
+    var trades = [];
+    _.each(team1.players, (player1) => {
+      _.each(team2.players, (player2) => {
+        var team1AfterTrade = team1.afterTrade([player1], [player2]);
+        var team2AfterTrade = team2.afterTrade([player2], [player1]);
 
-				var trade = new Trade(
-					team1, 
-					team2, 
-					[player1], 
-					[player2], 
-					team1AfterTrade.expectedPointsRestOfSeason(currentWeek) - team1.expectedPointsRestOfSeason(currentWeek),
-					team2AfterTrade.expectedPointsRestOfSeason(currentWeek) - team2.expectedPointsRestOfSeason(currentWeek));
-				trades.push(trade);
-			});
-		});
+        var trade = new Trade(
+          team1, 
+          team2, 
+          [player1], 
+          [player2], 
+          team1AfterTrade.expectedPointsRestOfSeason(currentWeek) - team1.expectedPointsRestOfSeason(currentWeek),
+          team2AfterTrade.expectedPointsRestOfSeason(currentWeek) - team2.expectedPointsRestOfSeason(currentWeek));
+        trades.push(trade);
+      });
+    });
 
-		return trades;
-	}
+    return trades;
+  }
 
-	getAll2v2Trades(team1, team2, currentWeek) {
-		var trades = [];
-		_.each(team1.players, (team1player1) => {
-			_.each(_.without(team1.players, team1player1), (team1player2) => {
-				_.each(team2.players, (team2player1) => {
-					_.each(_.without(team2.players, team2player1), (team2player2) => {
-						var team1AfterTrade = team1.afterTrade([team1player1, team1player2], [team2player1, team2player2]);
-						var team2AfterTrade = team2.afterTrade([team2player1, team2player2], [team1player1, team1player2]);
+  getAll2v2Trades(team1, team2, currentWeek) {
+    var trades = [];
+    _.each(team1.players, (team1player1) => {
+      _.each(_.without(team1.players, team1player1), (team1player2) => {
+        _.each(team2.players, (team2player1) => {
+          _.each(_.without(team2.players, team2player1), (team2player2) => {
+            var team1AfterTrade = team1.afterTrade([team1player1, team1player2], [team2player1, team2player2]);
+            var team2AfterTrade = team2.afterTrade([team2player1, team2player2], [team1player1, team1player2]);
 
-						var trade = new Trade(
-							team1, 
-							team2, 
-							[team1player1, team1player2], 
-							[team2player1, team2player2], 
-							team1AfterTrade.expectedPointsRestOfSeason(currentWeek) - team1.expectedPointsRestOfSeason(currentWeek),
-							team2AfterTrade.expectedPointsRestOfSeason(currentWeek) - team2.expectedPointsRestOfSeason(currentWeek));
-						trades.push(trade);
-					});
-				});
-			});
-		});
+            var trade = new Trade(
+              team1, 
+              team2, 
+              [team1player1, team1player2], 
+              [team2player1, team2player2], 
+              team1AfterTrade.expectedPointsRestOfSeason(currentWeek) - team1.expectedPointsRestOfSeason(currentWeek),
+              team2AfterTrade.expectedPointsRestOfSeason(currentWeek) - team2.expectedPointsRestOfSeason(currentWeek));
+            trades.push(trade);
+          });
+        });
+      });
+    });
 
-		return trades;
-	}
+    return trades;
+  }
 
-	getAll2v1Trades(team1, team2, currentWeek) {
-		var trades = [];
-		_.each(team1.players, (team1player1) => {
-			_.each(_.without(team1.players, team1player1), (team1player2) => {
-				_.each(team2.players, (team2player1) => {
-						var team1AfterTrade = team1.afterTrade([team1player1, team1player2], [team2player1]);
-						var team2AfterTrade = team2.afterTrade([team2player1], [team1player1, team1player2]);
+  getAll2v1Trades(team1, team2, currentWeek) {
+    var trades = [];
+    _.each(team1.players, (team1player1) => {
+      _.each(_.without(team1.players, team1player1), (team1player2) => {
+        _.each(team2.players, (team2player1) => {
+            var team1AfterTrade = team1.afterTrade([team1player1, team1player2], [team2player1]);
+            var team2AfterTrade = team2.afterTrade([team2player1], [team1player1, team1player2]);
 
-						var trade = new Trade(
-							team1, 
-							team2, 
-							[team1player1, team1player2], 
-							[team2player1], 
-							team1AfterTrade.expectedPointsRestOfSeason(currentWeek) - team1.expectedPointsRestOfSeason(currentWeek),
-							team2AfterTrade.expectedPointsRestOfSeason(currentWeek) - team2.expectedPointsRestOfSeason(currentWeek));
-						trades.push(trade);
-				});
-			});
-		});
+            var trade = new Trade(
+              team1, 
+              team2, 
+              [team1player1, team1player2], 
+              [team2player1], 
+              team1AfterTrade.expectedPointsRestOfSeason(currentWeek) - team1.expectedPointsRestOfSeason(currentWeek),
+              team2AfterTrade.expectedPointsRestOfSeason(currentWeek) - team2.expectedPointsRestOfSeason(currentWeek));
+            trades.push(trade);
+        });
+      });
+    });
 
-		return trades;
-	}
+    return trades;
+  }
 }
 
 export default League;
--- src/trade/trade.js
+++ src/trade/trade.js
@@ -1,79 +1,79 @@
 import _ from 'lodash';
 
 class Trade {
-	constructor(
-		team1,
-		team2,
-		playersToTradeOnTeam1,
-		playersToTradeOnTeam2,
-		team1ExpectedPointsAdded,
-		team2ExpectedPointsAdded) {
-		this.team1 = team1;
-		this.team2 = team2;
-		this.playersToTradeOnTeam1 = playersToTradeOnTeam1;
-		this.playersToTradeOnTeam2 = playersToTradeOnTeam2;
-		this.team1ExpectedPointsAdded = team1ExpectedPointsAdded;
-		this.team2ExpectedPointsAdded = team2ExpectedPointsAdded;
-		this.team1TradeScore = this.calculateTeam1Score(team1ExpectedPointsAdded, team2ExpectedPointsAdded);
-		this.team2TradeScore = this.calculateTeam1Score(team2ExpectedPointsAdded, team1ExpectedPointsAdded);
-		this.overallTradeScore = this.calculateOverallScore(team1ExpectedPointsAdded, team2ExpectedPointsAdded);
+  constructor(
+    team1,
+    team2,
+    playersToTradeOnTeam1,
+    playersToTradeOnTeam2,
+    team1ExpectedPointsAdded,
+    team2ExpectedPointsAdded) {
+    this.team1 = team1;
+    this.team2 = team2;
+    this.playersToTradeOnTeam1 = playersToTradeOnTeam1;
+    this.playersToTradeOnTeam2 = playersToTradeOnTeam2;
+    this.team1ExpectedPointsAdded = team1ExpectedPointsAdded;
+    this.team2ExpectedPointsAdded = team2ExpectedPointsAdded;
+    this.team1TradeScore = this.calculateTeam1Score(team1ExpectedPointsAdded, team2ExpectedPointsAdded);
+    this.team2TradeScore = this.calculateTeam1Score(team2ExpectedPointsAdded, team1ExpectedPointsAdded);
+    this.overallTradeScore = this.calculateOverallScore(team1ExpectedPointsAdded, team2ExpectedPointsAdded);
 
-	}
+  }
 
-	includesTeam(team) {
-		return team.id == this.team1.id || team.id == this.team2.id;
-	}
+  includesTeam(team) {
+    return team.id == this.team1.id || team.id == this.team2.id;
+  }
 
-	tradeScoreForTeam(team) {
-		if (!this.includesTeam(team)) {
-			throw `Can't get tradeScore for team ${team.id} because that team is not involved in this trade.`
-		} else if (team.id == this.team1.id) {
-			return this.team1TradeScore;
-		} else if (team.id == this.team2.id) {
-			return this.team2TradeScore;
-		}
-	}
+  tradeScoreForTeam(team) {
+    if (!this.includesTeam(team)) {
+      throw `Can't get tradeScore for team ${team.id} because that team is not involved in this trade.`
+    } else if (team.id == this.team1.id) {
+      return this.team1TradeScore;
+    } else if (team.id == this.team2.id) {
+      return this.team2TradeScore;
+    }
+  }
 
-	// Weighted in favor of team1.
-	// TODO tune
-	calculateTeam1Score(team1ExpectedPointsAdded, team2ExpectedPointsAdded) {
+  // Weighted in favor of team1.
+  // TODO tune
+  calculateTeam1Score(team1ExpectedPointsAdded, team2ExpectedPointsAdded) {
 
-		// Make sure both teams benefit.
-		if (team1ExpectedPointsAdded <= 0 || team2ExpectedPointsAdded <= 0) {
-			return -1;
-		} else {
-			// Bias towards team1, but make sure discrepancy is not too high. Add 100 so that we don't go below 0 (todo adjust)
-			return 100 + 0.8*team1ExpectedPointsAdded + 0.2*team2ExpectedPointsAdded - 0.8 * Math.abs(team1ExpectedPointsAdded - team2ExpectedPointsAdded);
-		}
-	}
+    // Make sure both teams benefit.
+    if (team1ExpectedPointsAdded <= 0 || team2ExpectedPointsAdded <= 0) {
+      return -1;
+    } else {
+      // Bias towards team1, but make sure discrepancy is not too high. Add 100 so that we don't go below 0 (todo adjust)
+      return 100 + 0.8*team1ExpectedPointsAdded + 0.2*team2ExpectedPointsAdded - 0.8 * Math.abs(team1ExpectedPointsAdded - team2ExpectedPointsAdded);
+    }
+  }
 
-	// Weighted in favor of overall trade fairness + impact.
-	// TODO tune
-	calculateOverallScore(team1ExpectedPointsAdded, team2ExpectedPointsAdded) {
-		// Make sure both teams benefit.
-		if (team1ExpectedPointsAdded <= 0 || team2ExpectedPointsAdded <= 0) {
-			return -1;
-		} else {
-			return 100 + 0.5*team1ExpectedPointsAdded + 0.5*team2ExpectedPointsAdded - 0.8 * Math.abs(team1ExpectedPointsAdded - team2ExpectedPointsAdded);
-		}
-	}
+  // Weighted in favor of overall trade fairness + impact.
+  // TODO tune
+  calculateOverallScore(team1ExpectedPointsAdded, team2ExpectedPointsAdded) {
+    // Make sure both teams benefit.
+    if (team1ExpectedPointsAdded <= 0 || team2ExpectedPointsAdded <= 0) {
+      return -1;
+    } else {
+      return 100 + 0.5*team1ExpectedPointsAdded + 0.5*team2ExpectedPointsAdded - 0.8 * Math.abs(team1ExpectedPointsAdded - team2ExpectedPointsAdded);
+    }
+  }
 
-	toString() {
-		var tradeString = "\n" +
-			`team1: ${this.team1.id} team2: ${this.team2.id} \n` +
-			`playersToTradeOnTeam1: ${_.map(this.playersToTradeOnTeam1, (player) => player.fullName)} \n` +
-			`playersToTradeOnTeam2: ${_.map(this.playersToTradeOnTeam2, (player) => player.fullName)} \n` +
-			`team1ExpectedPointsAdded: ${this.team1ExpectedPointsAdded} \n` +
-			`team2ExpectedPointsAdded: ${this.team2ExpectedPointsAdded} \n` +
-			`team1TradeScore: ${this.team1TradeScore} \n` +
-			`team2TradeScore: ${this.team2TradeScore} \n` +
-			`overallTradeScore: ${this.overallTradeScore}\n`;
-		if (this.team1ExpectedPointsAdded <= 0 || this.team2ExpectedPointsAdded <= 0) {
-			return `ONE TEAM LOSES. ${tradeString}`;
-		} else {
-			return `BOTH TEAMS WIN! ${tradeString}`;
-		}
-	}
+  toString() {
+    var tradeString = "\n" +
+      `team1: ${this.team1.id} team2: ${this.team2.id} \n` +
+      `playersToTradeOnTeam1: ${_.map(this.playersToTradeOnTeam1, (player) => player.fullName)} \n` +
+      `playersToTradeOnTeam2: ${_.map(this.playersToTradeOnTeam2, (player) => player.fullName)} \n` +
+      `team1ExpectedPointsAdded: ${this.team1ExpectedPointsAdded} \n` +
+      `team2ExpectedPointsAdded: ${this.team2ExpectedPointsAdded} \n` +
+      `team1TradeScore: ${this.team1TradeScore} \n` +
+      `team2TradeScore: ${this.team2TradeScore} \n` +
+      `overallTradeScore: ${this.overallTradeScore}\n`;
+    if (this.team1ExpectedPointsAdded <= 0 || this.team2ExpectedPointsAdded <= 0) {
+      return `ONE TEAM LOSES. ${tradeString}`;
+    } else {
+      return `BOTH TEAMS WIN! ${tradeString}`;
+    }
+  }
 }
 
 export default Trade;
